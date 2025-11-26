f986cc0db634b3bf4bd39eb582daee1b0c6b8680
pengcheng2017 <1031633784@qq.com>
2025-11-26 20:40:23 +0800

    add register page

--- src/features/FeatureJumbotronSection.tsx
+++ src/features/FeatureJumbotronSection.tsx
@@ -79,7 +79,7 @@ export default function FeatureJumbotronSection() {
                             <p className="text-sm text-left text-white flex-1">
                                 {Language(dictionary.try_for_free_subtitle_section)}
                             </p>
-                            <a href="#" className="text-[#EF3BFB] font-plus-jakarta-sans-medium hover:underline flex items-center gap-2 mt-2">
+                            <a href="/register" className="text-[#EF3BFB] font-plus-jakarta-sans-medium hover:underline flex items-center gap-2 mt-2">
                                 {Language(dictionary.try_for_free_button_section)}
                                 <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
                                     <path d="M8 0L6.59 1.41L12.17 7H0V9H12.17L6.59 14.59L8 16L16 8L8 0Z" />
